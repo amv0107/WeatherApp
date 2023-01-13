3156f2fc247603c4added6d99d3b62d231b4b6e6
amv0107 <amv0107@gmail.com>
2023-01-13 23:56:42 +0200

    permissions in the manifest added dependencies create an empty ViewModel class

---
+++ app/src/main/java/com/amv0107/weatherapp/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.amv0107.weatherapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+
+    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataList = MutableLiveData<String>()
+
+}
