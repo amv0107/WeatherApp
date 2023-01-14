65b5e8f1319df572ca5978d4b8293b727f70ad0e
amv0107 <amv0107@gmail.com>
2023-01-14 23:06:40 +0200

    created data class WeatherModel and item layout for list list_item.xml

--- app/src/main/java/com/amv0107/weatherapp/MainViewModel.kt
+++ app/src/main/java/com/amv0107/weatherapp/MainViewModel.kt
@@ -2,9 +2,10 @@ package com.amv0107.weatherapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.amv0107.weatherapp.adapters.WeatherModel
 
 class MainViewModel : ViewModel(){
 
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
