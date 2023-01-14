7653aacf385f6ff9d12520e534f883a492d56fba
amv0107 <amv0107@gmail.com>
2023-01-14 23:30:02 +0200

    created a RecyclerView Adapter, part 1

---
+++ app/src/main/java/com/amv0107/weatherapp/adapters/WeatherAdapter.kt
@@ -0,0 +1,20 @@
+package com.amv0107.weatherapp.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.amv0107.weatherapp.databinding.ListItemBinding
+
+class WeatherAdapter: ListAdapter<WeatherModel, WeatherAdapter.Holder>() {
+
+    class Holder(view: View): RecyclerView.ViewHolder(view){
+        val binding = ListItemBinding.bind(view)
+
+        fun bind(item: WeatherModel) = with(binding){
+            tvDate.text = item.time
+            tvCondition.text = item.condition
+            tvTemp.text = item.currentTemp
+        }
+    }
+
+}
