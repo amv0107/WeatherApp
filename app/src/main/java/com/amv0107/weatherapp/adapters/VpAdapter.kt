11018001e2c6f604fa1d67dac4fd294aac99b2ff
amv0107 <amv0107@gmail.com>
2023-01-14 13:12:05 +0200

    statusBar Color; HoursFragment; DaysFragment; VpAdapter; Connecting the adapter to viewPager2 and TabLayout

---
+++ app/src/main/java/com/amv0107/weatherapp/adapters/VpAdapter.kt
@@ -0,0 +1,13 @@
+package com.amv0107.weatherapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class VpAdapter(fa: FragmentActivity, private val list: List<Fragment>): FragmentStateAdapter(fa) {
+    override fun getItemCount() = list.size
+
+    override fun createFragment(position: Int): Fragment {
+        return list[position]
+    }
+}
--- app/src/main/java/com/amv0107/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/amv0107/weatherapp/fragments/MainFragment.kt
@@ -9,11 +9,22 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.amv0107.weatherapp.R
+import com.amv0107.weatherapp.adapters.VpAdapter
 import com.amv0107.weatherapp.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
 
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "HOURS",
+        "DAYS"
+    )
     private lateinit var binding: FragmentMainBinding
     private lateinit var pLauncher: ActivityResultLauncher<String>
 
@@ -28,6 +39,16 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+            tab, pos ->
+                tab.text = tList[pos]
+        }.attach()
     }
 
     private fun permissionListener(){
